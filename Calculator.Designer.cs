bab64243b3c80e4afade9ca76fbe549c6161d7e1
Dayvison Rocha <dayvison.rocha@hotmail.com>
2024-03-03 10:59:05 -0300

    Criação de uma calculadora simples com Windows Form

---
+++ Calculator.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnLess = new System.Windows.Forms.Button();
+            this.btnCross = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(115, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumber1.Location = new System.Drawing.Point(107, 111);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(175, 23);
+            this.txtNumber1.TabIndex = 1;
+            this.txtNumber1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumber2.Location = new System.Drawing.Point(107, 209);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(175, 23);
+            this.txtNumber2.TabIndex = 2;
+            this.txtNumber2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(217)))), ((int)(((byte)(88)))));
+            this.btnPlus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlus.FlatAppearance.BorderSize = 0;
+            this.btnPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlus.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPlus.Location = new System.Drawing.Point(120, 161);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(32, 26);
+            this.btnPlus.TabIndex = 3;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnLess
+            // 
+            this.btnLess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(217)))), ((int)(((byte)(88)))));
+            this.btnLess.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLess.FlatAppearance.BorderSize = 0;
+            this.btnLess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLess.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLess.Location = new System.Drawing.Point(159, 161);
+            this.btnLess.Name = "btnLess";
+            this.btnLess.Size = new System.Drawing.Size(32, 26);
+            this.btnLess.TabIndex = 4;
+            this.btnLess.Text = "-";
+            this.btnLess.UseVisualStyleBackColor = false;
+            this.btnLess.Click += new System.EventHandler(this.btnLess_Click);
+            // 
+            // btnCross
+            // 
+            this.btnCross.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(217)))), ((int)(((byte)(88)))));
+            this.btnCross.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCross.FlatAppearance.BorderSize = 0;
+            this.btnCross.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCross.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCross.Location = new System.Drawing.Point(198, 161);
+            this.btnCross.Name = "btnCross";
+            this.btnCross.Size = new System.Drawing.Size(32, 26);
+            this.btnCross.TabIndex = 5;
+            this.btnCross.Text = "x";
+            this.btnCross.UseVisualStyleBackColor = false;
+            this.btnCross.Click += new System.EventHandler(this.btnCross_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(217)))), ((int)(((byte)(88)))));
+            this.btnDivision.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDivision.FlatAppearance.BorderSize = 0;
+            this.btnDivision.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDivision.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDivision.Location = new System.Drawing.Point(237, 161);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(32, 26);
+            this.btnDivision.TabIndex = 6;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = false;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(217)))), ((int)(((byte)(88)))));
+            this.btnResult.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnResult.FlatAppearance.BorderSize = 0;
+            this.btnResult.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResult.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnResult.Location = new System.Drawing.Point(159, 263);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(71, 26);
+            this.btnResult.TabIndex = 7;
+            this.btnResult.Text = "=";
+            this.btnResult.UseVisualStyleBackColor = false;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtResult.Location = new System.Drawing.Point(107, 318);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(175, 23);
+            this.txtResult.TabIndex = 8;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.ClientSize = new System.Drawing.Size(392, 402);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnCross);
+            this.Controls.Add(this.btnLess);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNumber1;
+        private TextBox txtNumber2;
+        private Button btnPlus;
+        private Button btnLess;
+        private Button btnCross;
+        private Button btnDivision;
+        private Button btnResult;
+        private TextBox txtResult;
+    }
+}
---
+++ Calculator.cs
@@ -0,0 +1,94 @@
+using System.Reflection;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            btnDivision.BackColor = Color.FromArgb(125, 217, 88);
+            btnLess.BackColor = Color.FromArgb(125, 217, 88);
+            btnCross.BackColor = Color.FromArgb(125, 217, 88);
+            btnPlus.BackColor = Color.White;
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            btnPlus.BackColor = Color.FromArgb(125, 217, 88);
+            btnLess.BackColor = Color.FromArgb(125, 217, 88);
+            btnCross.BackColor = Color.FromArgb(125, 217, 88);
+            btnDivision.BackColor = Color.White;
+        }
+
+        private void btnLess_Click(object sender, EventArgs e)
+        {
+            btnPlus.BackColor = Color.FromArgb(125, 217, 88);
+            btnDivision.BackColor = Color.FromArgb(125, 217, 88);
+            btnCross.BackColor = Color.FromArgb(125, 217, 88);
+            btnLess.BackColor = Color.White;
+        }
+
+        private void btnCross_Click(object sender, EventArgs e)
+        {
+            btnPlus.BackColor = Color.FromArgb(125, 217, 88);
+            btnLess.BackColor = Color.FromArgb(125, 217, 88);
+            btnDivision.BackColor = Color.FromArgb(125, 217, 88);
+            btnCross.BackColor = Color.White;
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(txtNumber1.Text);
+            int n2 = int.Parse(txtNumber2.Text);
+
+
+            if (btnPlus.BackColor == Color.White)
+            {
+                txtResult.Text = Sum(n1, n2).ToString();
+
+            } else if (btnLess.BackColor == Color.White)
+            {
+                txtResult.Text = Less(n1, n2).ToString();
+
+            }else if(btnCross.BackColor == Color.White)
+            {
+                txtResult.Text = Cross(n1, n2).ToString();
+
+            }else if(btnDivision.BackColor == Color.White)
+            {
+                txtResult.Text = Division(n1, n2).ToString();
+            }
+
+            
+        }
+
+        public int Sum(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        public int Less(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+
+        public int Cross(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+
+        public int Division(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return result;
+        }
+    }
+}
